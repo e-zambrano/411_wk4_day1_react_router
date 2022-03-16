db6105038b766210ef8fc8e44b1c0bbb84e4b543
Erik Zambrano <ezambrano@onetrust.com>
2022-03-16 14:05:43 -0500

    allow user to navigate across the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
-// Write imports for Router & BrowserRouter here //
+import React from "react";
+import Navigation from "./components/Navigation";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+
+      {/* I moved what Router.js file has for easier visualization */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/car/:id" component={Car} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Chip from "@material-ui/core/Chip";
 
-const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+const Car = () => {
+  //I am able to use function useParams
+  //because I updated react-router-dom to 5.3.0
+  //instead of using props.match.params.id
+  const { id } = useParams();
+  const car = cars.find((c) => c.id === +id);
 
-export default Car
+  return (
+    <Container maxWidth="sm" className="car-container">
+      <Paper className="car-paper">
+        <h2>{car.Name}</h2>
+        {Object.keys(car).map((key, idx) => {
+          return <Chip key={idx} label={`${key}: ${car[key]}`}></Chip>;
+        })}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Car;
